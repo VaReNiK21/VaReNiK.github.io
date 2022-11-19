7264a2c497413e865a56f4477c91e4299d0ce6d4
Bondarenko Stas <107764927+VaReNiK21@users.noreply.github.com>
2022-11-19 22:17:40 +0200

    Update main2.js

--- Homework№3/main2.js
+++ Homework№3/main2.js
@@ -69,3 +69,148 @@ const profileIcon=new THREE.Mesh(iconGeometry, profileMaterial);
 const locationIcon=new THREE.Mesh(iconGeometry, locationMaterial);
 const leftIcon=new THREE.Mesh(iconGeometry, leftMaterial);
 const rightIcon=new THREE.Mesh(iconGeometry, rightMaterial);
+const portfolioItem0Video = await loadVideo("sintel.mp4");
+portfolioItem0Video.muted = true;
+const portfolioItem0VideoTexture = new
+THREE.VideoTexture(portfolioItem0Video);
+const portfolioItem0VideoMaterial = new
+THREE.MeshBasicMaterial({map: portfolioItem0VideoTexture});
+	const portfolioItem0Material = new
+THREE.MeshBasicMaterial({map: portfolioItem0Texture});
+const portfolioItem1Material = new
+THREE.MeshBasicMaterial({map: portfolioItem1Texture});
+const portfolioItem2Material = new
+THREE.MeshBasicMaterial({map: portfolioItem2Texture});
+
+const portfolioItem0V = new
+THREE.Mesh(planeGeometry, portfolioItem0VideoMaterial);
+const portfolioItem0 = new
+THREE.Mesh(planeGeometry, portfolioItem0Material);
+const portfolioItem1 = new
+THREE.Mesh(planeGeometry, portfolioItem1Material);
+const portfolioItem2 = new
+THREE.Mesh(planeGeometry, portfolioItem2Material);
+	profileIcon.position.set(-0.42, -0.5, 0);
+webIcon.position.set(-0.14, -0.5, 0);
+emailIcon.position.set(0.14, -0.5, 0);
+locationIcon.position.set(0.42, -0.5, 0);
+	const portfolioGroup = new THREE.Group();
+portfolioGroup.position.set(0, 0, -0.01);
+portfolioGroup.position.set(0, 0.6, -0.01);
+portfolioGroup.add(portfolioItem0);
+portfolioGroup.add(leftIcon);
+portfolioGroup.add(rightIcon);
+leftIcon.position.set(-0.7, 0, 0);
+rightIcon.position.set(0.7, 0, 0);
+	const avatar = await
+loadGLTF("https://raw.githubusercontent.com/AR-js-org/"+
+"AR.js/master/aframe/examples/image-tracking/nft/trex/"+
+"scene.gltf");
+avatar.scene.scale.set(0.02, 0.02, 0.02);
+avatar.scene.rotation.set(0, 11*Math.PI/180, 0);
+avatar.scene.position.set(-0.40, -0.45, -0.25);
+	const anchor = mindarThree.addAnchor(0);
+anchor.group.add(avatar.scene);
+anchor.group.add(card);
+anchor.group.add(emailIcon);
+anchor.group.add(webIcon);
+anchor.group.add(profileIcon);
+anchor.group.add(locationIcon);
+anchor.group.add(portfolioGroup);
+	const textElement = document.createElement("div");
+const textObj = new CSS3DObject(textElement);
+textObj.position.set(0, -1000, 0);
+textObj.visible = false;
+textElement.style.background = "#FFFFFF";
+textElement.style.padding = "30px";
+textElement.style.fontSize = "60px";
+	const cssAnchor = mindarThree.addCSSAnchor(0);
+cssAnchor.group.add(textObj);
+	leftIcon.userData.clickable = true;
+rightIcon.userData.clickable = true;
+emailIcon.userData.clickable = true;
+webIcon.userData.clickable = true;
+profileIcon.userData.clickable = true;
+locationIcon.userData.clickable = true;
+portfolioItem0.userData.clickable = true;
+portfolioItem0V.userData.clickable = true;
+portfolioItem1.userData.clickable = true;
+portfolioItem2.userData.clickable = true;
+	const portfolioItems = [portfolioItem0,
+portfolioItem1,
+portfolioItem2];
+let currentPortfolio = 0;
+	document.body.addEventListener("click", (e) => {
+const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
+const mouseY = -(e.clientY / window.innerHeight) * 2 + 1;
+const mouse = new THREE.Vector2(mouseX, mouseY);
+const raycaster = new THREE.Raycaster();
+raycaster.setFromCamera(mouse, camera);
+const intersects =
+raycaster.intersectObjects(scene.children, true);
+if (intersects.length > 0) {
+let o = intersects[0].object;
+while (o.parent && !o.userData.clickable)
+o = o.parent;
+if (o.userData.clickable) {
+if (o === leftIcon || o === rightIcon) {
+if (o === leftIcon)
+currentPortfolio = (currentPortfolio - 1 +
+portfolioItems.length) % portfolioItems.length;
+else
+currentPortfolio = (currentPortfolio + 1) %
+portfolioItems.length;
+portfolioItem0Video.pause();
+for (let i = 0; i < portfolioItems.length; i++)
+portfolioGroup.remove(portfolioItems[i]);
+portfolioGroup.add(portfolioItems[currentPortfolio]);
+}
+else if (o === portfolioItem0) {
+portfolioGroup.remove(portfolioItem0);
+portfolioGroup.add(portfolioItem0V);
+portfolioItems[0] = portfolioItem0V;
+portfolioItem0Video.play();
+}
+	else if (o === portfolioItem0V) {
+if (portfolioItem0Video.paused) {
+portfolioItem0Video.play();
+} else {
+portfolioItem0Video.pause();
+}
+}
+	else if (o === webIcon) {
+textObj.visible = true;
+textElement.innerHTML = "https://kdpu.edu.ua/semerikov";
+} else if (o === emailIcon) {
+textObj.visible = true;
+textElement.innerHTML = "semerikov [at] gmail.com";
+} else if (o === profileIcon) {
+textObj.visible = true;
+textElement.innerHTML =
+"https://www.researchgate.net/profile/Serhiy-Semerikov";
+} else if (o === locationIcon) {
+textObj.visible = true;
+textElement.innerHTML = "Kryvyi Rih, Ukraine";
+}
+	else if (o == portfolioItem1) {
+window.open("https://acnsci.org/journal");
+} else if (o == portfolioItem2) {
+window.open("https://acnsci.org/cte");
+}
+}
+}
+});
+	const clock = new THREE.Clock();
+await mindarThree.start();
+renderer.setAnimationLoop(() => {
+const delta = clock.getDelta();
+const elapsed = clock.getElapsedTime();
+const iconScale = 1 + 0.2 * Math.sin(elapsed*5);
+[webIcon, emailIcon, profileIcon, locationIcon].forEach((icon) => {
+icon.scale.set(iconScale, iconScale, iconScale);
+});
+	avatar.scene.position.set(-0.40,-0.45,-0.25+Math.sin(elapsed));
+	});
+}
+start();
+});
