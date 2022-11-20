1b3a8dce81fef35dd14930860cbf5cf14a1e3010
Bondarenko Stas <107764927+VaReNiK21@users.noreply.github.com>
2022-11-20 19:50:58 +0200

    Update main2.js

--- Homework№3/main2.js
+++ Homework№3/main2.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		const loader = new GLTFLoader();
 
 		loader.load("marker.glb", (gltf) => {
-			gltf.scene.scale.set(0.05, 0.05, 0.05);
+			gltf.scene.scale.set(0.01, 0.01, 0.01);
 			gltf.scene.position.set(0, 0, 0);
 			anchor.group.add(gltf.scene);
 		});
